41fc05bd47a9bd34d0f7cec672039247bb96f275
datlas460 <srihari.rd9@gmail.com>
2024-03-07 09:54:15 -0500

    Completed Scikit Vector back tester and started Linear Regression with Tensor Flow

--- sklinregpractice.py
+++ sklinregpractice.py
@@ -7,6 +7,11 @@ from pylab import mpl, plt
 import pandas as pd
 from sklearn import linear_model
 
+
+
+raw = pd.read_csv('http://hilpisch.com/pyalgo_eikon_eod_data.csv',
+                  index_col=0,
+                  parse_dates=True).dropna()
 x = np.arange(12)
 print(x)
 
@@ -69,6 +74,51 @@ plt.plot(hours, prob.T[1], 'g-.', label='$p(h)$ for one')
 plt.ylim(-0.2, 1.2)
 plt.legend(loc=0)
 plt.show()
+
+
+
+
+symbol = 'GLD'
+data = pd.DataFrame(raw[symbol])
+data.rename(columns={symbol: 'price'}, inplace=True)
+data['return'] = np.log(data['price'] / data['price'].shift(1))
+data.dropna(inplace=True)
+lags = 3
+cols = []
+for lag in range(1, lags + 1):
+    col = 'lag_{}'.format(lag)
+    data[col] = data['return'].shift(lag)
+    cols.append(col)
+data.dropna(inplace=True)
+
+
+from sklearn.metrics import accuracy_score
+
+lm = linear_model.LogisticRegression(C=1e7, solver='lbfgs',
+                                              multi_class='auto',
+                                              max_iter=1000)
+
+lm.fit(data[cols], np.sign(data['return']))
+
+
+data['prediction'] = lm.predict(data[cols])
+
+data['prediction'].value_counts()
+
+hits = np.sign(data['return'].iloc[lags:] *
+                         data['prediction'].iloc[lags:]
+                         ).value_counts()
+
+print(hits)
+accuracy_score(data['prediction'],
+                         np.sign(data['return']))
+data['strategy'] = data['prediction'] * data['return']
+data[['return', 'strategy']].sum().apply(np.exp)
+data[['return', 'strategy']].cumsum().apply(np.exp).plot(
+    figsize=(10, 6))
+plt.show()
+
+
 
 
 
